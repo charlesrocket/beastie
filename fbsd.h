cfb5eb0c9b6f3f2c007147a585e615c55edfa027
-k <slowdive@me.com>
2021-06-16 00:01:23 -0700

    Move fonts

--- fbsd.h
+++ fbsd.h
@@ -2,10 +2,10 @@
 #define FBSD_H
 
 #include <Watchy.h>
+#include "fonts/conso17pt7b.h"
+#include "fonts/conso12pt7b.h"
+#include "fonts/conso10pt7b.h"
 #include "daemon.h"
-#include "conso17pt7b.h"
-#include "conso12pt7b.h"
-#include "conso10pt7b.h"
 
 class WatchyFBSD : public Watchy{
     public:
